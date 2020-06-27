2b931d5e89d23a1a113f7754b77fbf2529f475f8
Ivan Vasilov <vasilov.ivan@gmail.com>
2020-06-27 19:11:40 +0200

    Simplify the database provider. Rely on the TypeORM resolving.

--- backend/src/database/provider.ts
+++ backend/src/database/provider.ts
@@ -1,24 +1,9 @@
-import { createConnection, ConnectionManager, ConnectionOptionsReader, ConnectionOptions } from 'typeorm';
+import { createConnection } from 'typeorm';
 import { DATABASE_CONNECTION } from '../constants';
 
 export const databaseProviders = [
   {
     provide: DATABASE_CONNECTION,
-    useFactory: async () => {
-      // This whole workaround is needed because typeorm has bugs in its connection options resolutions. I'm using
-      // ConnectionOptionsReader to read env vars (if they're defined). If not, just use the json in the package
-      // which can't be resolved because TypeORM fails at simple going up the directories until you find conf files.
-      let option: ConnectionOptions;
-      try {
-        const reader = new ConnectionOptionsReader();
-        let options = await reader.all();
-        console.log(options);
-        option = options[0];
-      } catch {
-        option = require('../../ormconfig.json');
-      }
-
-      return createConnection(option);
-    },
+    useFactory: async () => createConnection(),
   },
 ];
