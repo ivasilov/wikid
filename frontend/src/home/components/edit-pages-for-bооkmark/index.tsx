7764ee679d359f5c1697ecb815a46274c14b7355
Ivan Vasilov <vasilov.ivan@gmail.com>
2020-11-23 21:04:49 +0100

    Extract a the pages picker component.

---
+++ frontend/src/home/components/edit-pages-for-bооkmark/index.tsx
@@ -0,0 +1,111 @@
+import * as React from 'react';
+import { MenuItem } from '@blueprintjs/core';
+import { MultiSelect, ItemPredicate, IItemRendererProps } from '@blueprintjs/select';
+import { useGetAllPagesForDropdownQuery } from '../../../models';
+
+export type IdName = { id?: string; name: string };
+
+const PageMultiSelect = MultiSelect.ofType<IdName>();
+
+const renderNewTag = (query: string, active: boolean, handleClick: React.MouseEventHandler<HTMLElement>) => {
+  return (
+    <MenuItem
+      icon="add"
+      text={`Create "${query}"`}
+      active={active}
+      onClick={handleClick}
+      shouldDismissPopover={false}
+    />
+  );
+};
+
+const renderPage = (
+  page: IdName,
+  { modifiers, handleClick }: IItemRendererProps,
+  isPageSelected: (i: IdName) => boolean,
+) => {
+  if (!modifiers.matchesPredicate) {
+    return null;
+  }
+
+  if (isPageSelected(page)) {
+    return null;
+  }
+
+  return (
+    <MenuItem
+      key={page.name}
+      active={modifiers.active}
+      onClick={handleClick}
+      shouldDismissPopover={false}
+      text={page.name}
+    />
+  );
+};
+
+const filterPage: ItemPredicate<IdName> = (query, page, _index, exactMatch) => {
+  const normalizedName = page.name.toLowerCase();
+  const normalizedQuery = query.toLowerCase();
+
+  if (exactMatch) {
+    return normalizedName === normalizedQuery;
+  } else {
+    return normalizedName.indexOf(normalizedQuery) >= 0;
+  }
+};
+
+interface Props {
+  pages: IdName[];
+  onChange: (p: IdName[]) => void;
+}
+
+export const EditPagesForBookmark = (props: Props) => {
+  const { pages, onChange } = props;
+
+  const { data, loading } = useGetAllPagesForDropdownQuery({ fetchPolicy: 'network-only' });
+
+  if (loading) {
+    return <div>Loading</div>;
+  }
+
+  if (data?.currentUserPages) {
+    const availablePages = pages.filter(bp => !pages.find(p => p.id === bp.id));
+
+    const selectPage = (p: IdName) => {
+      const ps = pages.concat([p]);
+      onChange(ps);
+    };
+
+    const addPage = (name: string) => ({ name: name });
+    const removePage = (page: any) => {
+      const ps = pages.filter(p => p.name !== page);
+      onChange(ps);
+    };
+
+    const isPageSelected = (page: IdName) => {
+      return !!pages.find(p => p.id === page.id && p.name === page.name);
+    };
+
+    return (
+      <PageMultiSelect
+        fill
+        tagRenderer={p => p.name}
+        itemRenderer={(item, itemProps) => renderPage(item, itemProps, isPageSelected)}
+        itemPredicate={filterPage}
+        onItemSelect={selectPage}
+        createNewItemFromQuery={addPage}
+        createNewItemRenderer={renderNewTag}
+        items={availablePages}
+        selectedItems={pages}
+        noResults={<MenuItem disabled={true} text="No results." />}
+        popoverProps={{ minimal: true }}
+        tagInputProps={{
+          tagProps: { minimal: true },
+          onRemove: removePage,
+        }}
+      />
+    );
+  }
+
+  return <div>An error happened while trying to show possible pages for linking to this bookmark.</div>;
+};
--- frontend/src/home/components/editBookmark/index.tsx
+++ frontend/src/home/components/editBookmark/index.tsx
@@ -1,15 +1,10 @@
 import * as React from 'react';
 import { observer } from 'mobx-react';
-import { Dialog, Button, Classes, InputGroup, MenuItem, Spinner, FormGroup, Switch } from '@blueprintjs/core';
-import { MultiSelect, ItemPredicate, IItemRendererProps } from '@blueprintjs/select';
-import { action, observable, computed, runInAction } from 'mobx';
+import { Dialog, Button, Classes, InputGroup, Spinner, FormGroup, Switch } from '@blueprintjs/core';
+import { action, observable, runInAction } from 'mobx';
 import { uniqBy } from 'lodash';
 import Container from 'typedi';
-import { fromPromise } from 'mobx-utils';
 import {
-  gqlGetAllPagesForDropdownQuery,
-  GetAllPagesForDropdownDocument,
-  gqlGetAllPagesForDropdownQueryVariables,
   UpdateBookmarkDocument,
   gqlGetBookmarkQuery,
   gqlUpdateBookmarkMutation,
@@ -18,15 +13,12 @@ import {
   gqlGetBookmarkQueryVariables,
 } from '../../../models';
 import { GraphQLClient } from '../../../apolloClient';
-
-type IdName = { id?: string; name: string };
+import { EditPagesForBookmark, IdName } from '../edit-pages-for-bооkmark';
 
 interface Props {
   bookmark: { id: string };
   onClose: () => void;
 }
-
-const PageMultiSelect = MultiSelect.ofType<IdName>();
 
 class EditBookmarkDialogState {
   readonly props: Props;
@@ -65,37 +57,6 @@ class EditBookmarkDialogState {
   changePages = (p: IdName[]) => {
     let pages = p.map(p => ({ id: p.id, name: p.name }));
     this.pages = uniqBy(pages, p => p.name);
-  };
-
-  selectPage = (p: IdName) => {
-    this.changePages(this.pages.concat([p]));
-  };
-
-  @action
-  addPage = (name: string) => {
-    return { name: name };
-  };
-
-  @action removePage = (page: string) => {
-    this.pages = this.pages.filter(p => p.name !== page);
-  };
-
-  @computed get availablePages() {
-    const client = Container.get(GraphQLClient);
-    const pages = client
-      .query<gqlGetAllPagesForDropdownQuery, gqlGetAllPagesForDropdownQueryVariables>({
-        query: GetAllPagesForDropdownDocument,
-      })
-      .then(v => v.data.currentUserPages)
-      .then(pages => {
-        return pages.filter(bp => !this.pages.find(p => p.id === bp.id));
-      });
-
-    return fromPromise(pages);
-  }
-
-  isPageSelected = (page: IdName) => {
-    return this.pages.find(p => p.id === page.id && p.name === page.name);
   };
 
   init = () => {
@@ -157,49 +118,6 @@ class EditBookmarkDialogState {
   };
 }
 
-export const filterPage: ItemPredicate<IdName> = (query, page, _index, exactMatch) => {
-  const normalizedName = page.name.toLowerCase();
-  const normalizedQuery = query.toLowerCase();
-
-  if (exactMatch) {
-    return normalizedName === normalizedQuery;
-  } else {
-    return normalizedName.indexOf(normalizedQuery) >= 0;
-  }
-};
-
-const renderPage = (page: IdName, { modifiers, handleClick }: IItemRendererProps, state: EditBookmarkDialogState) => {
-  if (!modifiers.matchesPredicate) {
-    return null;
-  }
-
-  if (state.isPageSelected(page)) {
-    return null;
-  }
-
-  return (
-    <MenuItem
-      key={page.name}
-      active={modifiers.active}
-      onClick={handleClick}
-      shouldDismissPopover={false}
-      text={page.name}
-    />
-  );
-};
-
-const renderNewTag = (query: string, active: boolean, handleClick: React.MouseEventHandler<HTMLElement>) => {
-  return (
-    <MenuItem
-      icon="add"
-      text={`Create "${query}"`}
-      active={active}
-      onClick={handleClick}
-      shouldDismissPopover={false}
-    />
-  );
-};
-
 export const EditBookmarkDialog = observer((props: Props) => {
   const [state] = React.useState(() => new EditBookmarkDialogState(props));
 
@@ -218,28 +136,7 @@ export const EditBookmarkDialog = observer((props: Props) => {
             </FormGroup>
             <Switch checked={state.read} label="Read" onChange={state.changeReadFlag} />
             <FormGroup label="Pages which have this bookmark">
-              {state.availablePages.case({
-                pending: () => <div>Loading</div>,
-                fulfilled: availablePages => (
-                  <PageMultiSelect
-                    fill
-                    tagRenderer={p => p.name}
-                    itemRenderer={(item, itemProps) => renderPage(item, itemProps, state)}
-                    itemPredicate={filterPage}
-                    onItemSelect={state.selectPage}
-                    createNewItemFromQuery={state.addPage}
-                    createNewItemRenderer={renderNewTag}
-                    items={availablePages}
-                    selectedItems={state.pages}
-                    noResults={<MenuItem disabled={true} text="No results." />}
-                    popoverProps={{ minimal: true }}
-                    tagInputProps={{
-                      tagProps: { minimal: true },
-                      onRemove: state.removePage,
-                    }}
-                  />
-                ),
-              })}
+              <EditPagesForBookmark pages={state.pages} onChange={state.changePages} />
             </FormGroup>
           </div>
           <div className={Classes.DIALOG_FOOTER}>
