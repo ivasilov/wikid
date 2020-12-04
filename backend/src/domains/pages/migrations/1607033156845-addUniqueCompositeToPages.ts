efcf14f69dc7183ca294d5b38e46c50d0138a210
Ivan Vasilov <vasilov.ivan@gmail.com>
2020-12-04 01:51:37 +0100

    Run prettier on the new migration.

--- backend/src/domains/pages/migrations/1607033156845-addUniqueCompositeToPages.ts
+++ backend/src/domains/pages/migrations/1607033156845-addUniqueCompositeToPages.ts
@@ -1,16 +1,18 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class addUniqueCompositeToPages1607033156845 implements MigrationInterface {
-    name = 'addUniqueCompositeToPages1607033156845'
+  name = 'addUniqueCompositeToPages1607033156845';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "bookmarks" ALTER COLUMN "read" DROP DEFAULT`, undefined);
-        await queryRunner.query(`ALTER TABLE "pages" ADD CONSTRAINT "UQ_ec92c3fc29205a1fa2bbb4c9281" UNIQUE ("name", "userId")`, undefined);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "bookmarks" ALTER COLUMN "read" DROP DEFAULT`, undefined);
+    await queryRunner.query(
+      `ALTER TABLE "pages" ADD CONSTRAINT "UQ_ec92c3fc29205a1fa2bbb4c9281" UNIQUE ("name", "userId")`,
+      undefined,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "pages" DROP CONSTRAINT "UQ_ec92c3fc29205a1fa2bbb4c9281"`, undefined);
-        await queryRunner.query(`ALTER TABLE "bookmarks" ALTER COLUMN "read" SET DEFAULT true`, undefined);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "pages" DROP CONSTRAINT "UQ_ec92c3fc29205a1fa2bbb4c9281"`, undefined);
+    await queryRunner.query(`ALTER TABLE "bookmarks" ALTER COLUMN "read" SET DEFAULT true`, undefined);
+  }
 }
