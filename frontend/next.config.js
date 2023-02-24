3ed66669886565dea482327ff23b8d18649e182b
Ivan Vasilov <vasilov.ivan@gmail.com>
2023-02-24 01:02:50 +0100

    Add command for analyzing frontend bundle.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -16,4 +16,5 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+const withBundleAnalyzer = require('@next/bundle-analyzer')({ enabled: process.env.ANALYZE === 'true' });
+module.exports = withBundleAnalyzer(nextConfig);
